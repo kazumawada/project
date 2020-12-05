91ce28a68a754d24cd831e47fcf7c339860320f8
kazumawada <kazumawadaa@gmail.com>
2020-12-05 12:33:54 +0900

    create-staticPage-login-signup #21

--- front/src/views/Signup.vue
+++ front/src/views/Signup.vue
@@ -1,3 +1,125 @@
 <template>
-  <h1>Signup</h1>
+<div class="Signup">
+<h1>Signup</h1>
+<v-container>
+<v-row>
+<v-col
+cols="12"
+>
+
+<v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+    <v-select
+      v-model="select"
+      :items="items"
+      :rules="[v => !!v || 'Item is required']"
+      label="Password"
+      required
+    ></v-select>
+
+     <v-select
+      v-model="select"
+      :items="items"
+      :rules="[v => !!v || 'Item is required']"
+      label="Passwordの再確認"
+      required
+    ></v-select>
+   <p><a href="/login">oh! you already have an account hah?</a></p>
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="アカウント情報を記憶する"
+      required
+    ></v-checkbox>
+    
+    
+
+    <v-btn
+      :disabled="!valid"
+      color="blue"
+      class="mr-4"
+      @click="validate"
+    >
+      Create my account!
+    </v-btn>
+
+    <!-- <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      Reset Form
+    </v-btn> -->
+<!-- 
+    <v-btn
+      color="warning"
+      @click="resetValidation"
+    >
+      Reset Validation
+    </v-btn> -->
+  </v-form>
+
+</v-col>
+</v-row>
+</v-container>
+</div>
+<!-- end of .login -->
+
+  
 </template>
+<script>
+  export default {
+    el: '#Signup',
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 3) || 'Name must be less than 3 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ],
+      checkbox: false,
+    }),
+
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      },
+    },
+  }
+</script>
