714766342b1f084008c592f09216e8ead7c69d95
kazumawada <kazumawadaa@gmail.com>
2020-12-14 08:59:35 +0900

    create staticPage profile #30

--- front/src/views/Profile.vue
+++ front/src/views/Profile.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>
-    <div>
+  <v-card>
+    <v-system-bar></v-system-bar>
+    <v-toolbar flat>
+     <!-- userImage -->
+    <v-avatar>
       <img :src="$auth.user.picture">
-      <h2>{{ $auth.user.name }}</h2>
-      <p>{{ $auth.user.email }}</p>
-    </div>
-
-    <div>
-      <pre>{{ JSON.stringify($auth.user, null, 2) }}</pre>
-    </div>
-  </div>
+    </v-avatar>
+      <!-- userName -->
+      <v-toolbar-title>{{ $auth.user.nickname }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div>
+      <v-btn
+        text
+        color="deep-purple accent-4"
+      >
+        Follow
+      </v-btn>
+       <v-btn
+        text
+        color="deep-purple accent-4"
+      >
+        Edit
+      </v-btn>
+      </div>
+    </v-toolbar>
+    <v-banner
+      single-line
+      :sticky="sticky"
+    >
+      description here.
+    </v-banner>
+    <v-card-text class="grey lighten-4">
+      <v-sheet
+        max-width="800"
+        height="300"
+        class="mx-auto"
+      ></v-sheet>
+    </v-card-text>
+  </v-card>
 </template>
+<script>
+  export default {
+    data: () => ({
+      sticky: false,
+    }),
+  }
+</script>
+
